86b1744607af91721a792108352f0e6daa44ff10
TRBC <therealbitcoin.club@protonmail.com>
2020-11-10 19:57:18 -0400

    added integration tests

--- test/integration/bfp-integration.js
+++ test/integration/bfp-integration.js
@@ -10,54 +10,14 @@ const sinon = require('sinon')
 // Locally global variables.
 const assert = chai.assert
 
-// Mocking data libraries.
-const mockData = require('./mocks/bfp-mocks')
-
 // Unit under test
 const BFP = require('../../lib/bfp')
-
 const bfp = new BFP()
+const Bfp = require('bitcoinfiles-node').bfp
 
 describe('#bfp.js', () => {
-  let sandbox
-
-  // Restore the sandbox before each test.
-  beforeEach(() => (sandbox = sinon.createSandbox()))
-  afterEach(() => sandbox.restore())
-
-  describe('#writeBfp', () => {
-    it('should write a file to the blockchain', async () => {
-      /*sandbox
-        .stub(uut.bchjs.Electrumx, 'balance')
-        .resolves(mockData.electrumBalance)
-
-      sandbox
-        .stub(uut.bchjs.Crypto, 'sha256')
-        .resolves("n16vM3Ndnpp43GzDnxhHgshMCXspkvsJoq")
-
-      sandbox
-        .stub(Bfp, 'calculateFileUploadCost')
-        .resolves(1042)
-
-      sandbox
-        .stub(uut.bchjs.Electrumx, 'utxo')
-        .resolves(mockData.electrumUTXOs)*/
-
-      /*
-            sandbox  //TODO I WANT TO STUB THE CALL IN _uploadToBlockchain but getting: TypeError: Cannot stub non-existent property uploadFile
-              .stub(bfp, 'uploadFile')
-              .resolves('fileId8739873984')*/
-
-
-      assert.isNotEmpty(await bfp.writeBFP())
-    })
-  })
-
   describe('#getUTXOsByAddress', () => {
     it('should get UTXOs on an address', async () => {
-      sandbox
-        .stub(bfp.bchjs.Electrumx, 'utxo')
-        .resolves(mockData.electrumUTXOs)
       const addr = 'bchtest:qrtddel54p4zxmrkf7jyex7j06lhx48k3s5wqpgu5p'
       //const addr = 'bitcoincash:qqh793x9au6ehvh7r2zflzguanlme760wuzehgzjh9'
 
@@ -74,10 +34,6 @@ describe('#bfp.js', () => {
 
   describe('#getBCHBalance', () => {
     it('should get BCH balance on an address', async () => {
-      sandbox
-        .stub(bfp.bchjs.Electrumx, 'balance')
-        .resolves(mockData.electrumBalance)
-
       const addr = 'bchtest:qrtddel54p4zxmrkf7jyex7j06lhx48k3s5wqpgu5p'
       //const addr = 'bitcoincash:qqh793x9au6ehvh7r2zflzguanlme760wuzehgzjh9'
 
@@ -85,6 +41,12 @@ describe('#bfp.js', () => {
 
       assert.equal(balance, 0.01)
       //assert.equal(balance, 0.00001) MAINNET
+    })
+  })
+
+  describe('#writeBfp', () => {
+    it('should write a file to the blockchain', async () => {
+      assert.isNotEmpty(await bfp.writeBFP())
     })
   })
 })
