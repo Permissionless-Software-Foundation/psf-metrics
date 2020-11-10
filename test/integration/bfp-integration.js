ba16f036268b2f66ad6a50e9eee39382867cdc7b
TRBC <therealbitcoin.club@protonmail.com>
2020-11-10 17:25:14 -0400

    chmod 644

---
+++ test/integration/bfp-integration.js
@@ -0,0 +1,90 @@
+/*
+  Unit tests for the bfp.js Bitcoin Files library.
+*/
+
+// npm libraries
+const chai = require('chai')
+const sinon = require('sinon')
+//const Bfp = require('bitcoinfiles-node').bfp
+
+// Locally global variables.
+const assert = chai.assert
+
+// Mocking data libraries.
+const mockData = require('./mocks/bfp-mocks')
+
+// Unit under test
+const BFP = require('../../lib/bfp')
+
+const bfp = new BFP()
+
+describe('#bfp.js', () => {
+  let sandbox
+
+  // Restore the sandbox before each test.
+  beforeEach(() => (sandbox = sinon.createSandbox()))
+  afterEach(() => sandbox.restore())
+
+  describe('#writeBfp', () => {
+    it('should write a file to the blockchain', async () => {
+      /*sandbox
+        .stub(uut.bchjs.Electrumx, 'balance')
+        .resolves(mockData.electrumBalance)
+
+      sandbox
+        .stub(uut.bchjs.Crypto, 'sha256')
+        .resolves("n16vM3Ndnpp43GzDnxhHgshMCXspkvsJoq")
+
+      sandbox
+        .stub(Bfp, 'calculateFileUploadCost')
+        .resolves(1042)
+
+      sandbox
+        .stub(uut.bchjs.Electrumx, 'utxo')
+        .resolves(mockData.electrumUTXOs)*/
+
+      /*
+            sandbox  //TODO I WANT TO STUB THE CALL IN _uploadToBlockchain but getting: TypeError: Cannot stub non-existent property uploadFile
+              .stub(bfp, 'uploadFile')
+              .resolves('fileId8739873984')*/
+
+
+      assert.isNotEmpty(await bfp.writeBFP())
+    })
+  })
+
+  describe('#getUTXOsByAddress', () => {
+    it('should get UTXOs on an address', async () => {
+      sandbox
+        .stub(bfp.bchjs.Electrumx, 'utxo')
+        .resolves(mockData.electrumUTXOs)
+      const addr = 'bchtest:qrtddel54p4zxmrkf7jyex7j06lhx48k3s5wqpgu5p'
+      //const addr = 'bitcoincash:qqh793x9au6ehvh7r2zflzguanlme760wuzehgzjh9'
+
+      const utxos = await bfp.getUTXOsByAddress(addr)
+      console.log(utxos)
+
+      // Assert essential UTXOs properties exist.
+      assert.isArray(utxos.utxos)
+      assert.property(utxos.utxos[0], 'tx_hash')
+      assert.property(utxos.utxos[0], 'height')
+      assert.property(utxos.utxos[0], 'value')
+    })
+  })
+
+  describe('#getBCHBalance', () => {
+    it('should get BCH balance on an address', async () => {
+      sandbox
+        .stub(bfp.bchjs.Electrumx, 'balance')
+        .resolves(mockData.electrumBalance)
+
+      const addr = 'bchtest:qrtddel54p4zxmrkf7jyex7j06lhx48k3s5wqpgu5p'
+      //const addr = 'bitcoincash:qqh793x9au6ehvh7r2zflzguanlme760wuzehgzjh9'
+
+      const balance = await bfp.getBCHBalance(addr, true)
+
+      assert.equal(balance, 0.01)
+      //assert.equal(balance, 0.00001) MAINNET
+    })
+  })
+})
---
+++ test/unit/mocks/bfp-mocks.js
@@ -0,0 +1,78 @@
+/*
+  A mocking library for util.js unit tests.
+  A mocking library contains data to use in place of the data that would come
+  from an external dependency.
+*/
+
+'use strict'
+
+const electrumUTXOs = {
+  success: true,
+  utxos: [{
+      height: 602405,
+      tx_hash: 'lkhgfidsghds7hz98w4hz98fusdsfdsfdshduifhudsfhdsfhoFAKEHASH',
+      tx_pos: 0,
+      value: 1000
+    },
+    {
+      height: 602405,
+      tx_hash: '2b37bdb3b63dd0bca720437754a36671431a950e684b64c44ea910ea9d5297c7',
+      tx_pos: 1,
+      value: 2000
+    },
+    {
+      height: 602405,
+      tx_hash: 'lkhgfidsghds7hz98w4hz98fushduifhudsfhdsfhoFAKEHASH',
+      tx_pos: 2,
+      value: 3000
+    }
+  ]
+}
+
+/*
+//TODO Clarify the differences between this and the UTXO model above
+const electrumUTXOs = {
+  utxos: [{
+      txid: "2b37bdb3b63dd0bca720437754a36671431a950e684b64c44ea910ea9d5297c7",
+      vout: 0,
+      amount: 0.00001,
+      satoshis: 1000,
+      height: 602405,
+      confirmations: 36459
+    },
+    {
+      txid: "2b37bdb3b63dd0bca720437754a36671431a950e684b64c44ea910ea9d5297c7",
+      vout: 0,
+      amount: 0.00003,
+      satoshis: 3000,
+      height: 602405,
+      confirmations: 36459
+    },
+    {
+      txid: "2b37bdb3b63dd0bca720437754a36671431a950e684b64c44ea910ea9d5297c7",
+      vout: 0,
+      amount: 0.00002,
+      satoshis: 2000,
+      height: 602405,
+      confirmations: 36459
+    }
+  ],
+  legacyAddress: "15NCRBJsHaJy8As5bX1oh2YauRejnZ1MKF",
+  cashAddress: "bitcoincash:qqh793x9au6ehvh7r2zflzguanlme760wuzehgzjh9",
+  slpAddress: "simpleledger:qqh793x9au6ehvh7r2zflzguanlme760wuwzunhjfm",
+  scriptPubKey: "76a9142fe2c4c5ef359bb2fe1a849f891cecffbcfb4f7788ac",
+  asm: "OP_DUP OP_HASH160 2fe2c4c5ef359bb2fe1a849f891cecffbcfb4f77 OP_EQUALVERIFY OP_CHECKSIG"
+} */
+
+const electrumBalance = {
+  "success": true,
+  "balance": {
+    "confirmed": 90000000,
+    "unconfirmed": 10000000
+  }
+}
+
+module.exports = {
+  electrumUTXOs,
+  electrumBalance
+}
