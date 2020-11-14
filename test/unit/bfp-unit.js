2a0c6aed143c1898d474c989260a3a2a2b796aa8
Chris Troutner <chris.troutner@gmail.com>
2020-11-13 18:28:18 -0800

    Refactored unit tests and writeBFP code

--- test/unit/bfp-unit.js
+++ test/unit/bfp-unit.js
@@ -15,23 +15,29 @@ const mockData = require('./mocks/bfp-mocks')
 
 // Unit under test
 const BFP = require('../../lib/bfp')
-const bfp = new BFP()
-// const Bfp = require('bitcoinfiles-node').bfp
+let uut
+
+const Bfp = require('bitcoinfiles-node').bfp
 
 describe('#bfp.js', () => {
   let sandbox
 
   // Restore the sandbox before each test.
-  beforeEach(() => (sandbox = sinon.createSandbox()))
+  beforeEach(() => {
+    sandbox = sinon.createSandbox()
+
+    uut = new BFP()
+  })
+
   afterEach(() => sandbox.restore())
 
   describe('#getUTXOsByAddress', () => {
     it('should get UTXOs on an address', async () => {
-      sandbox.stub(bfp.bchjs.Electrumx, 'utxo').resolves(mockData.electrumUTXOs)
+      sandbox.stub(uut.bchjs.Electrumx, 'utxo').resolves(mockData.electrumUTXOs)
       const addr = 'bchtest:qrtddel54p4zxmrkf7jyex7j06lhx48k3s5wqpgu5p'
       // const addr = 'bitcoincash:qqh793x9au6ehvh7r2zflzguanlme760wuzehgzjh9'
 
-      const utxos = await bfp.getUTXOsByAddress(addr)
+      const utxos = await uut.getUTXOsByAddress(addr)
       // console.log(utxos)
 
       // Assert essential UTXOs properties exist.
@@ -47,12 +53,12 @@ describe('#bfp.js', () => {
       try {
         // Force an error.
         sandbox
-          .stub(bfp.bchjs.Electrumx, 'balance')
+          .stub(uut.bchjs.Electrumx, 'balance')
           .rejects(new Error('Unsupported address format : asdf'))
 
         const addr = 'asdf'
 
-        await bfp.getBCHBalance(addr)
+        await uut.getBCHBalance(addr)
 
         assert.fail()
       } catch (e) {
@@ -62,12 +68,12 @@ describe('#bfp.js', () => {
 
     it('should get BCH balance on an address', async () => {
       sandbox
-        .stub(bfp.bchjs.Electrumx, 'balance')
+        .stub(uut.bchjs.Electrumx, 'balance')
         .resolves(mockData.electrumBalance)
 
       const addr = 'bitcoincash:qqh793x9au6ehvh7r2zflzguanlme760wuzehgzjh9'
 
-      const balance = await bfp.getBCHBalance(addr, true)
+      const balance = await uut.getBCHBalance(addr, true)
 
       assert.equal(balance, 1)
     })
@@ -76,7 +82,7 @@ describe('#bfp.js', () => {
   describe('#_throwErrorIfBalanceIsInSufficient', () => {
     it('should throw an error if balance is insufficient', async () => {
       try {
-        await bfp._throwErrorIfBalanceIsInSufficient(1, 2)
+        await uut._throwErrorIfBalanceIsInSufficient(1, 2)
 
         assert.fail()
       } catch (err) {
@@ -88,80 +94,86 @@ describe('#bfp.js', () => {
     })
   })
 
-  // describe('#getUTXOsByAddress', () => {
-  //   it('should throw an error as address is invalid', async () => {
-  //     sandbox.stub(bfp.bchjs.Electrumx, 'utxo').resolves({
-  //       utxos: []
-  //     })
-  //     try {
-  //       await bfp.getUTXOsByAddress('')
-  //       assert.fail()
-  //     } catch (e) {
-  //       assert.equal(e, 'Error: No UTXOs found.')
-  //     }
-  //   })
-  // })
-  //
-  // describe('#initWallet', () => {
-  //   it('should throw an error if wallet file is not found', async () => {
-  //     try {
-  //       sandbox.stub(bfp, 'openWallet').throws(new Error())
-  //       bfp.initWallet()
-  //     } catch (e) {
-  //       assert.include(
-  //         e.message,
-  //         'Could not open wallet.json. Add a mainnet wallet.json file.'
-  //       )
-  //     }
-  //   })
-  // })
-  //
-  // describe('#checkBalanceKillProcessIfZero', () => {
-  //   it('should kill process as balance is 0.0', async () => {
-  //     try {
-  //       await bfp.checkBalanceKillProcessIfZero(0.0)
-  //       assert.fail()
-  //     } catch (e) {
-  //       assert.equal(e, 'Error: Balance of sending address is zero.')
-  //     }
-  //   })
-  // })
-  //
-  // describe('#writeBfp', () => {
-  //   it('should throw error as addr is invalid', async () => {
-  //     try {
-  //       const error = new Error('some fake error')
-  //       sandbox.stub(bfp, 'getBCHBalance').throws(error)
-  //       await bfp.writeBFP(true)
-  //       assert.fail()
-  //     } catch (e) {
-  //       assert.notEqual(e.message, 'assert.fail()')
-  //       assert.notEqual(
-  //         e,
-  //         'TypeError: Cannot stub non-existent property SEND_ADDR'
-  //       )
-  //       assert.equal(e, 'Error: some fake error')
-  //     }
-  //   })
-  //
-  //   it('should write a file to the blockchain', async () => {
-  //     sandbox
-  //       .stub(bfp.bchjs.Electrumx, 'balance')
-  //       .resolves(mockData.electrumBalance)
-  //
-  //     sandbox
-  //       .stub(bfp.bchjs.Crypto, 'sha256')
-  //       .resolves('n16vM3Ndnpp43GzDnxhHgshMCXspkvsJoq')
-  //
-  //     sandbox
-  //       .stub(Bfp, 'calculateFileUploadCost') // this is a static function
-  //       .resolves(1042)
-  //
-  //     sandbox.stub(bfp.bchjs.Electrumx, 'utxo').resolves(mockData.electrumUTXOs)
-  //
-  //     sandbox.stub(bfp.bfp, 'uploadFile').resolves('fileId8739873984')
-  //
-  //     assert.isNotEmpty(await bfp.writeBFP())
-  //   })
-  // })
+  describe('#getUTXOsByAddress', () => {
+    it('should throw an error if address is invalid', async () => {
+      try {
+        // Force an error.
+        sandbox.stub(uut.bchjs.Electrumx, 'utxo').resolves({
+          utxos: []
+        })
+
+        await uut.getUTXOsByAddress('')
+        assert.fail()
+      } catch (err) {
+        assert.include(err.message, 'No UTXOs found.')
+      }
+    })
+  })
+
+  describe('#initWallet', () => {
+    it('should throw an error if wallet file is not found', async () => {
+      try {
+        // Force an error.
+        sandbox.stub(uut, 'openWallet').throws(new Error())
+
+        uut.initWallet()
+      } catch (err) {
+        assert.include(
+          err.message,
+          'Could not open wallet.json. Add a mainnet wallet.json file.'
+        )
+      }
+    })
+  })
+
+  describe('#checkBalanceKillProcessIfZero', () => {
+    it('should kill process as balance is 0.0', async () => {
+      try {
+        uut.checkBalanceKillProcessIfZero(0.0)
+
+        assert.fail('Unexpected result')
+      } catch (err) {
+        // console.log('err: ', err)
+        assert.include(err.message, 'Balance of sending address is zero.')
+      }
+    })
+  })
+
+  describe('#writeBfp', () => {
+    it('should throw error if addr is invalid', async () => {
+      try {
+        // Froce an error
+        const error = new Error('some fake error')
+        sandbox.stub(uut, 'getBCHBalance').rejects(error)
+
+        await uut.writeBFP(true)
+
+        assert.fail()
+      } catch (err) {
+        assert.include(err.message, 'some fake error')
+      }
+    })
+
+    it('should write a file to the blockchain', async () => {
+      sandbox
+        .stub(uut.bchjs.Electrumx, 'balance')
+        .resolves(mockData.electrumBalance)
+
+      sandbox
+        .stub(uut.bchjs.Crypto, 'sha256')
+        .resolves('n16vM3Ndnpp43GzDnxhHgshMCXspkvsJoq')
+
+      sandbox
+        .stub(Bfp, 'calculateFileUploadCost') // this is a static function
+        .resolves(1042)
+
+      sandbox.stub(uut.bchjs.Electrumx, 'utxo').resolves(mockData.electrumUTXOs)
+
+      sandbox.stub(uut.bfp, 'uploadFile').resolves('fileId8739873984')
+
+      const result = await uut.writeBFP()
+
+      assert.isString(result)
+    })
+  })
 })
