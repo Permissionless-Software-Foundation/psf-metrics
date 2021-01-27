e9ba876c5bf80519afdb2a40adba31496c2a26e8
Chris Troutner <chris.troutner@gmail.com>
2021-01-27 14:34:10 -0800

    Prototyping metrics

---
+++ test/integration/metrics-integration.js
@@ -0,0 +1,21 @@
+/*
+  Integration tests for the metrics.js library.
+*/
+
+const Metrics = require('../../lib/metrics')
+let uut
+
+describe('#metrics', () => {
+  beforeEach(() => {
+    uut = new Metrics()
+  })
+
+  describe('#tokenLiquidityBurn', () => {
+    it('should do something', async () => {
+      const Dec1stBlock = 664000
+      const Dec31stBlock = 668300
+
+      await uut.tokenLiquidityBurn(Dec1stBlock, Dec31stBlock)
+    })
+  })
+})
