db81cd169e359563fda2e1ccac603f00c91394f3
Chris Troutner <chris.troutner@gmail.com>
2021-01-30 16:41:29 -0800

    Ran all reports for Nov 2020 post fork

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ class BoilplateLib {
 
     for (let i = 0; i < data.length; i++) {
       const thisData = data[i]
-      outStr += `${thisData.date},${thisData.height},${thisData.amount},${thisData.txid},${thisData.vout}\n`
+      outStr += `${thisData.date},${thisData.height},${thisData.amount},${
+        thisData.txid
+      },${thisData.vout}\n`
     }
 
     return outStr
@@ -40,44 +42,20 @@ async function runReport () {
   try {
     const lib = new BoilplateLib()
 
-    const Dec1stBlock = 664000
-    const Dec31stBlock = 668300
-    // const inflows = await lib.metrics.tokenInflows(Dec1stBlock, Dec31stBlock)
+    const startBlock = 659650
+    const stopBlock = 664000
+
+    // const inflows = await lib.metrics.tokenInflows(startBlock, stopBlock)
     // const csv = lib.generateCSV(inflows)
     // console.log(csv)
 
-    // const outflows = await lib.metrics.tokenOutflows(Dec1stBlock, Dec31stBlock)
+    // const outflows = await lib.metrics.tokenOutflows(startBlock, stopBlock)
     // const csv = lib.generateCSV(outflows)
     // console.log(csv)
 
-    // await lib.metrics.tokenBurns(672210, 672215)
-    const burns = await lib.metrics.tokenBurns(Dec1stBlock, Dec31stBlock)
+    const burns = await lib.metrics.tokenBurns(startBlock, stopBlock)
     const csv = lib.generateCSV(burns)
     console.log(csv)
-
-    // console.log(
-    //   `Token inflows between blocks ${Oct1stBlock} and ${Oct30thBlock}:\n${JSON.stringify(
-    //     inflows,
-    //     null,
-    //     2
-    //   )}`
-    // )
-
-    // const Nov1stBlock = 659634
-    // const Nov30thBlock = 668300
-    // const inflows = await lib.metrics.tokenInflows(Nov1stBlock, Nov30thBlock)
-
-    // const Oct1stBlock = 655220
-    // const Oct30thBlock = 659634
-    // const inflows = await lib.metrics.tokenInflows(Oct1stBlock, Oct30thBlock)
-    //
-    // console.log(
-    //   `Token inflows between blocks ${Oct1stBlock} and ${Oct30thBlock}:\n${JSON.stringify(
-    //     inflows,
-    //     null,
-    //     2
-    //   )}`
-    // )
   } catch (err) {
     console.error('Error in runReport(): ', err)
   }
